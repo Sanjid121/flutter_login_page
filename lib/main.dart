7656462866ca94c370dd6f92ad23215b5d4078d2
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-09-10 13:02:19 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_page/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
